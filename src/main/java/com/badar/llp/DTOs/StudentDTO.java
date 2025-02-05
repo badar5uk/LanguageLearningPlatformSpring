9eafa479b98ae0873963fc7d03ef4bdb09955dfb
Badar <bader5uk@hotmail.com>
2025-02-05 11:16:21 +0400

    Adjusted ERD

--- src/main/java/com/badar/llp/DTOs/StudentDTO.java
+++ src/main/java/com/badar/llp/DTOs/StudentDTO.java
@@ -1,5 +1,22 @@
 package com.badar.llp.DTOs;
 
 public class StudentDTO {
-    
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
