72fc92c40059e8cd0e643c8988ee6c52ba703be0
Badar <bader5uk@hotmail.com>
2025-02-05 12:37:39 +0400

    Added database connection and added missing annotations

--- src/main/java/com/badar/llp/Repositories/TutorRepository.java
+++ src/main/java/com/badar/llp/Repositories/TutorRepository.java
@@ -2,6 +2,8 @@ package com.badar.llp.Repositories;
 
 import com.badar.llp.Models.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TutorRepository extends JpaRepository<Tutor,Integer> {
 }
--- src/main/java/com/badar/llp/Repositories/VideoRepository.java
+++ src/main/java/com/badar/llp/Repositories/VideoRepository.java
@@ -2,6 +2,8 @@ package com.badar.llp.Repositories;
 
 import com.badar.llp.Models.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Video,Integer> {
 }
