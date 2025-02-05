19611d6ef7d4fd7a450e31dad54ad8d734d84067
Badar <bader5uk@hotmail.com>
2025-02-05 14:20:41 +0400

    Added updateddate, createddate and isactive attributes

--- src/main/java/com/badar/llp/Models/BasicModel.java
+++ src/main/java/com/badar/llp/Models/BasicModel.java
@@ -4,11 +4,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 public class BasicModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
+    private Date createdDate;
+    private Date updatedDate;
+    private Boolean isActive;
 
     public Integer getId() {
         return id;
@@ -24,5 +29,29 @@ public class BasicModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
     }
 }
