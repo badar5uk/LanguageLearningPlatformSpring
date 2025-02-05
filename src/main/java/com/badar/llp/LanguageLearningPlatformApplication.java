588e95e9bd100754853f2c105381bc13b016e6a6
Badar <bader5uk@hotmail.com>
2025-02-05 10:12:36 +0400

    intial project commit

---
+++ src/main/java/com/badar/llp/LanguageLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.badar.llp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearningPlatformApplication.class, args);
+	}
+
+}
