5a679cabb3ab830565e6e818732032f5b0b8dedb
Badar <bader5uk@hotmail.com>
2025-02-05 11:31:21 +0400

    Added full DTO functions and adjusted ERD

--- src/main/java/com/badar/llp/DTOs/StudentDTO.java
+++ src/main/java/com/badar/llp/DTOs/StudentDTO.java
@@ -1,5 +1,11 @@
 package com.badar.llp.DTOs;
 
+import com.badar.llp.Models.Student;
+import com.badar.llp.Utils.HelperUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentDTO {
     private Integer id;
     private String name;
@@ -18,5 +24,41 @@ public class StudentDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public StudentDTO convertToDTO(Student student) {
+        StudentDTO dto = new StudentDTO();
+        dto.setId(student.getId());
+        dto.setName(student.getName());
+        return dto;
+    }
+
+    public Student convertFromDTO(StudentDTO dto) {
+        Student student = new Student();
+        student.setId(dto.getId());
+        student.setName(dto.getName());
+        return student;
+    }
+
+    public List<StudentDTO> convertToDTO(List<Student> studentList) {
+        List<StudentDTO> studentDTOList = new ArrayList<>();
+        if (HelperUtils.isNotNull(studentList)) {
+            for (Student student : studentList) {
+                studentDTOList.add(convertToDTO(student));
+                return studentDTOList;
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public List<Student> convertFromDTO(List<StudentDTO> studentDTOList) {
+        List<Student> studentList = new ArrayList<>();
+        if (HelperUtils.isNotNull(studentDTOList)) {
+            for (StudentDTO studentDTO : studentDTOList) {
+                studentList.add(convertFromDTO(studentDTO));
+                return studentList;
+            }
+        }
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/badar/llp/DTOs/TutorDTO.java
+++ src/main/java/com/badar/llp/DTOs/TutorDTO.java
@@ -1,4 +1,65 @@
 package com.badar.llp.DTOs;
 
+import com.badar.llp.Models.Student;
+import com.badar.llp.Models.Tutor;
+import com.badar.llp.Utils.HelperUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TutorDTO {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TutorDTO convertToDTO(Tutor tutor) {
+        TutorDTO dto = new TutorDTO();
+        dto.setId(tutor.getId());
+        dto.setName(tutor.getName());
+        return dto;
+    }
+
+    public Tutor convertFromDTO(TutorDTO dto) {
+        Tutor tutor = new Tutor();
+        tutor.setId(dto.getId());
+        tutor.setName(dto.getName());
+        return tutor;
+    }
+
+    public List<TutorDTO> convertToDTO(List<Tutor> tutorList) {
+        List<TutorDTO> studentDTOList = new ArrayList<>();
+        if (HelperUtils.isNotNull(tutorList)) {
+            for (Tutor tutor : tutorList) {
+                studentDTOList.add(convertToDTO(tutor));
+                return studentDTOList;
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public List<Tutor> convertFromDTO(List<TutorDTO> tutorDTOList) {
+        List<Tutor> tutorList = new ArrayList<>();
+        if (HelperUtils.isNotNull(tutorDTOList)) {
+            for (TutorDTO tutorDTO : tutorDTOList) {
+                tutorList.add(convertFromDTO(tutorDTO));
+                return tutorList;
+            }
+        }
+        return new ArrayList<>();
+    }
 }
