bd1700df2b294661652f54ed102699193882f642
Badar <bader5uk@hotmail.com>
2025-02-05 12:27:51 +0400

    Added Repositories to use JPA Repository

---
+++ src/main/java/com/badar/llp/Repositories/TutorRepository.java
@@ -0,0 +1,7 @@
+package com.badar.llp.Repositories;
+
+import com.badar.llp.Models.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorRepository extends JpaRepository<Tutor,Integer> {
+}
