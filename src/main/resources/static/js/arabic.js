c11cecad203333d45161a854f6f5ffa2da9cb791
Badar <bader5uk@hotmail.com>
2025-02-09 22:59:48 +0400

    added fetch function to return list of json

--- src/main/resources/static/js/arabic.js
+++ src/main/resources/static/js/arabic.js
@@ -1,12 +1,29 @@
-const ararbicVideoList = [];
+const fetchLink = "http://localhost:8080/language/1";
+async function fetchVideos(fetchLink) {
+    let response = await fetch(fetchLink).
+    then((result) => result.json()).
+    then((language) => language.videoDTOList);
+    console.log(response);
+    return response;
+}
+ararbicVideoList = fetchVideos(fetchLink);
 
-ararbicVideoList.push(fetch("http://localhost:8080/language/1").
-then((response) => response.json()).
-then((result) => result.videoDTOList));
+ararbicVideoList.array.forEach(element => {
+    console.log(element.link);
+});
+console.log(ararbicVideoList);
+
+/*
+ararbicVideoList = await fetch("http://localhost:8080/language/1").
+then((response) => console.log(response.json())).
+then((result) => result.videoDTOList).then((video) => video.link);
+
+setTimeout(console.log(ararbicVideoList), 2000);
+
 
 setTimeout(ararbicVideoList.forEach(video => {
     console.log(video.link);
 }), 2000);
-
+*/
 
 
