c754a2e4996100673da238f28b6e555e1801691f
Badar <bader5uk@hotmail.com>
2025-02-11 13:58:08 +0400

    adjusted the controller to return the proper response

--- src/main/java/com/badar/llp/Controllers/ChatBotController.java
+++ src/main/java/com/badar/llp/Controllers/ChatBotController.java
@@ -1,21 +1,27 @@
 package com.badar.llp.Controllers;
 
+
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatBotController {
 
-    private final ChatClient chatClient;
 
-    public ChatBotController(ChatClient chatClient) {
-        this.chatClient = chatClient;
+    private ChatClient chatClient;
+
+    public ChatBotController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
     }
 
-    @GetMapping("/ai")
-    public String generation(@RequestParam(value ="message", defaultValue = "Tell me th best language to learn") String message) {
-        return chatClient.prompt(message).toString();
+
+    @RequestMapping(value = "/ai", method = RequestMethod.GET)
+    public String generation(@RequestParam(value = "message", defaultValue = "Tell me th best language to learn") String message) {
+        return chatClient.prompt().user(message)
+                .call()
+                .content();
     }
 }
