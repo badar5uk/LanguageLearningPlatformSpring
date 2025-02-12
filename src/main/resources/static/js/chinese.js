cc5cc074870ea19af0e7881c97f3d21496f93916
Badar <bader5uk@hotmail.com>
2025-02-12 13:51:09 +0400

    added the fetch for chinese, english and spanish in html and js

---
+++ src/main/resources/static/js/chinese.js
@@ -0,0 +1,92 @@
+const chineseLinkList = [];
+const chineseNameList = [];
+
+ async function getLinks() {
+    let response = await fetch("http://localhost:8080/language/2").
+    then((result) => result.json()).
+    then((language) => language.videoDTOList);
+    response.forEach(element => {
+        chineseLinkList.push(element.link)
+    });
+    return response;
+}
+ async function fetchVideoNames() {
+    let response = await fetch("http://localhost:8080/language/2").
+    then((result) => result.json()).
+    then((language) => language.videoDTOList);
+    response.forEach(element => {
+        chineseNameList.push(element.name)
+    });
+    return response;
+}
+
+
+//const btn = document.createElement("button");
+//btn.innerHTML = '<iframe width="560" height="315" src="'.stripslashes($row['videourl']).'" frameborder="0" allowfullscreen></iframe>';
+
+//btn.innerHTML = '<img src="http://img.youtube.com/ZMpekpfglxA?si=BqzAABfDOoKD5jSW.jpg" title="YouTube Video" alt="YouTube Video" />'
+//document.body.appendChild(btn);
+console.log(chineseLinkList);
+
+async function buildTable() {
+
+await getLinks();
+await fetchVideoNames();
+    
+const table = document.createElement('table');
+table.style.border = '1px solid black';
+
+// Create a header row for the table
+const headerRow = document.createElement('tr');
+
+const headerCell1 = document.createElement('th');
+headerCell1.textContent = 'Chinese Link';
+headerRow.appendChild(headerCell1);
+
+const headerCell2 = document.createElement('th');
+headerCell2.textContent = 'Name';
+headerRow.appendChild(headerCell2);
+
+// Append the header row to the table
+table.appendChild(headerRow);
+
+console.log("List :".concat(chineseLinkList));
+
+// Loop through the arrays to create table rows
+for (let i = 0; i < chineseLinkList.length; i++) {
+    const row = document.createElement('tr');
+  const cell1 = document.createElement('td');
+
+  // Create the cell for the Arabic link
+
+//   const link = document.createElement('button');
+//   link.href = arabicLinkList[i];
+//   link.textContent = arabicLinkList[i];
+//   //cell1.src = link;
+//   link.target = "_blank";
+//   cell1.appendChild(link);
+//   row.appendChild(cell1);
+
+const button = document.createElement('iframe');
+button.src = chineseLinkList[i]; 
+
+button.onclick = function() {
+  window.open(chineseLinkList[i], "_blank");
+};
+cell1.appendChild(button);
+row.appendChild(cell1);
+
+  // Create the cell for the Name
+  const cell2 = document.createElement('td');
+  cell2.textContent = chineseNameList[i];
+  row.appendChild(cell2);
+
+  // Append the row to the table
+  table.appendChild(row);
+}
+
+// Append the table to the body of the HTML document
+document.body.appendChild(table);
+ }
+
+ buildTable();
---
+++ src/main/resources/static/js/spanish.js
@@ -0,0 +1,92 @@
+const spanishLinkList = [];
+const spanishNameList = [];
+
+ async function getLinks() {
+    let response = await fetch("http://localhost:8080/language/103").
+    then((result) => result.json()).
+    then((language) => language.videoDTOList);
+    response.forEach(element => {
+        spanishLinkList.push(element.link)
+    });
+    return response;
+}
+ async function fetchVideoNames() {
+    let response = await fetch("http://localhost:8080/language/103").
+    then((result) => result.json()).
+    then((language) => language.videoDTOList);
+    response.forEach(element => {
+        spanishNameList.push(element.name)
+    });
+    return response;
+}
+
+
+//const btn = document.createElement("button");
+//btn.innerHTML = '<iframe width="560" height="315" src="'.stripslashes($row['videourl']).'" frameborder="0" allowfullscreen></iframe>';
+
+//btn.innerHTML = '<img src="http://img.youtube.com/ZMpekpfglxA?si=BqzAABfDOoKD5jSW.jpg" title="YouTube Video" alt="YouTube Video" />'
+//document.body.appendChild(btn);
+console.log(spanishLinkList);
+
+async function buildTable() {
+
+await getLinks();
+await fetchVideoNames();
+    
+const table = document.createElement('table');
+table.style.border = '1px solid black';
+
+// Create a header row for the table
+const headerRow = document.createElement('tr');
+
+const headerCell1 = document.createElement('th');
+headerCell1.textContent = 'Spanish Link';
+headerRow.appendChild(headerCell1);
+
+const headerCell2 = document.createElement('th');
+headerCell2.textContent = 'Name';
+headerRow.appendChild(headerCell2);
+
+// Append the header row to the table
+table.appendChild(headerRow);
+
+console.log("List :".concat(spanishLinkList));
+
+// Loop through the arrays to create table rows
+for (let i = 0; i < spanishLinkList.length; i++) {
+    const row = document.createElement('tr');
+  const cell1 = document.createElement('td');
+
+  // Create the cell for the Arabic link
+
+//   const link = document.createElement('button');
+//   link.href = arabicLinkList[i];
+//   link.textContent = arabicLinkList[i];
+//   //cell1.src = link;
+//   link.target = "_blank";
+//   cell1.appendChild(link);
+//   row.appendChild(cell1);
+
+const button = document.createElement('iframe');
+button.src = spanishLinkList[i]; 
+
+button.onclick = function() {
+  window.open(spanishLinkList[i], "_blank");
+};
+cell1.appendChild(button);
+row.appendChild(cell1);
+
+  // Create the cell for the Name
+  const cell2 = document.createElement('td');
+  cell2.textContent = spanishNameList[i];
+  row.appendChild(cell2);
+
+  // Append the row to the table
+  table.appendChild(row);
+}
+
+// Append the table to the body of the HTML document
+document.body.appendChild(table);
+ }
+
+ buildTable();
