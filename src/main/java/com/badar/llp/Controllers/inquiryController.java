1737e443632f87782b8c37672bf8ba14bd7881f2
Badar <bader5uk@hotmail.com>
2025-02-20 09:23:29 +0400

    added all the logic for the controller and the service

--- src/main/java/com/badar/llp/Controllers/inquiryController.java
+++ src/main/java/com/badar/llp/Controllers/inquiryController.java
@@ -1,17 +1,37 @@
 package com.badar.llp.Controllers;
 
 import com.badar.llp.DTOs.InquiryDTO;
-import com.badar.llp.Models.Inquiry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.badar.llp.DTOs.LanguageDTO;
+import com.badar.llp.Services.InquiryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "inquiry")
 public class inquiryController {
 
-//    @GetMapping(value = "getAll")
-//    public InquiryDTO getAllInquiries(){
-//
-//    }
+    @Autowired
+    InquiryService inquiryService;
+
+    @GetMapping(value = "getAll")
+    public List<InquiryDTO> getAllInquiries(){
+        return inquiryService.getAllInquiries();
+    }
+
+    @GetMapping(value = "{id}")
+    public InquiryDTO getInquiryById(@RequestParam Integer id){
+        return inquiryService.getInquiryById(id);
+    }
+
+    @PostMapping
+    public  InquiryDTO addInquiry(@RequestBody InquiryDTO inquiryDTO){
+        return inquiryService.addInquiry(inquiryDTO);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public String deleteInquiryById(@PathVariable Integer id, @RequestBody InquiryDTO inquiryDTO){
+        return inquiryService.deleteInquiryById(id,inquiryDTO);
+    }
 }
--- src/main/java/com/badar/llp/Services/InquiryService.java
+++ src/main/java/com/badar/llp/Services/InquiryService.java
@@ -1,11 +1,15 @@
 package com.badar.llp.Services;
 
 import com.badar.llp.DTOs.InquiryDTO;
+import com.badar.llp.DTOs.VideoDTO;
 import com.badar.llp.Models.Inquiry;
+import com.badar.llp.Models.Video;
 import com.badar.llp.Repositories.InquiryRepository;
+import com.badar.llp.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,5 +21,28 @@ public class InquiryService {
     public List<InquiryDTO> getAllInquiries(){
         List<Inquiry> inquiryList = inquiryRepository.findAll();
         return InquiryDTO.convertToDTO(inquiryList);
+    }
+
+    public InquiryDTO getInquiryById(Integer id){
+        return InquiryDTO.convertToDTO(inquiryRepository.getById(id));
+    }
+
+    public InquiryDTO addInquiry(InquiryDTO dto){
+        Inquiry entity = InquiryDTO.convertFromDTO(dto);
+        entity.setActive(true);
+        entity.setCreatedDate(new Date());
+        inquiryRepository.save(entity);
+        InquiryDTO inquiryDTO = InquiryDTO.convertToDTO(entity);
+        return inquiryDTO;
+    }
+
+    public String deleteInquiryById(Integer id, InquiryDTO inquiryDTO){
+        if(HelperUtils.isNotNull(id)){
+            Inquiry entity = InquiryDTO.convertFromDTO(inquiryDTO);
+            entity.setActive(false);
+            inquiryRepository.save(entity);
+            return "Deleted";
+        }
+        return "Not found";
     }
 }
