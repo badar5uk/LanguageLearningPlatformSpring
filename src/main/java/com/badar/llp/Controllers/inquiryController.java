3852eed7d665e46b650239b17f2ead02846b52a6
Badar <bader5uk@hotmail.com>
2025-02-20 08:38:21 +0400

    Added logic for inquiry in backend to log inquiries

---
+++ src/main/java/com/badar/llp/Controllers/inquiryController.java
@@ -0,0 +1,17 @@
+package com.badar.llp.Controllers;
+
+import com.badar.llp.DTOs.InquiryDTO;
+import com.badar.llp.Models.Inquiry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "inquiry")
+public class inquiryController {
+
+//    @GetMapping(value = "getAll")
+//    public InquiryDTO getAllInquiries(){
+//
+//    }
+}
