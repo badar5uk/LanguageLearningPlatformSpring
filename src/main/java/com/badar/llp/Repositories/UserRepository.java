6b8d9b9319ec0cc08856fcb4f8d95ad175d9bcdd
Badar <bader5uk@hotmail.com>
2025-02-24 11:37:09 +0400

    Added query to find user by username

--- src/main/java/com/badar/llp/Repositories/UserRepository.java
+++ src/main/java/com/badar/llp/Repositories/UserRepository.java
@@ -2,6 +2,12 @@ package com.badar.llp.Repositories;
 
 import com.badar.llp.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    @Query(value = "SELECT userName from User Where userName ")
+    User findByUserName(String userName);
 }
