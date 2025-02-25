dc23f5ff24561e162124bb98ce5bd2fc1e875af6
Badar <bader5uk@hotmail.com>
2025-02-25 23:30:39 +0400

    Added Jwt service

---
+++ src/main/java/com/badar/llp/Services/JwtService.java
@@ -0,0 +1,31 @@
+package com.badar.llp.Services;
+
+import com.badar.llp.DTOs.UserDTO;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private String secretKey = null;
+
+
+  public JwtBuilder generateToken(UserDTO dto){
+      Map<String,Object> map = new HashMap<>();
+      return Jwts.builder().addClaims(map).setSubject(dto.getUserName())
+              .setIssuer("LLP")
+              .setIssuedAt(new Date(System.currentTimeMillis()))
+              .setExpiration(new Date(System.currentTimeMillis() + 60*10*1000))
+              .signWith(SignatureAlgorithm.HS256, getSecretKey());
+   }
+
+    public String getSecretKey(){
+        return System.getenv("SECRET_HIDDEN_KEY");
+    }
+}
