209372629fb9a50df7c155f19307c53dccc5b556
Badar <bader5uk@hotmail.com>
2025-02-25 08:45:44 +0400

    Added UserDTO

---
+++ src/main/java/com/badar/llp/DTOs/UserDTO.java
@@ -0,0 +1,111 @@
+package com.badar.llp.DTOs;
+
+import com.badar.llp.Models.Role;
+import com.badar.llp.Models.Student;
+import com.badar.llp.Models.User;
+import com.badar.llp.Utils.HelperUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+    private String userName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static UserDTO convertToDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setEmail(user.getEmail());
+        dto.setUserName(user.getUsername());
+        dto.setName(user.getName());
+        dto.setPassword(user.getPassword());
+        dto.setRole(user.getRole());
+        return dto;
+    }
+
+    public static User convertFromDTO(UserDTO dto) {
+        User user = new User();
+        user.setId(dto.getId());
+        user.setEmail(dto.getEmail());
+        user.setUsername(dto.getUserName());
+        user.setName(dto.getName());
+        user.setRole(dto.getRole());
+        user.setPassword(dto.getPassword());
+        return user;
+    }
+
+    private static List<UserDTO> convertToDTO(List<User> userList) {
+        List<UserDTO> dtoList = new ArrayList<>();
+        if (HelperUtils.isNotNull(userList)) {
+            for (User user : userList) {
+                dtoList.add(convertToDTO(user));
+            }
+            return dtoList;
+        }
+        return new ArrayList<>();
+    }
+
+    private static List<User> convertFromDTO(List<UserDTO> dtoList){
+        List<User> userList = new ArrayList<>();
+        if (HelperUtils.isNotNull(dtoList)){
+            for(UserDTO dto : dtoList){
+                userList.add(convertFromDTO(dto));
+            }
+            return userList;
+        }
+        return new ArrayList<>();
+    }
+
+}
