e46a6f3e55dfa6e0b7eada5c1bb19418953b18bc
Badar <bader5uk@hotmail.com>
2025-03-02 11:04:24 +0400

    added getter and setter in languag response

--- src/main/java/com/badar/llp/Responses/LanguageResponse.java
+++ src/main/java/com/badar/llp/Responses/LanguageResponse.java
@@ -2,4 +2,12 @@ package com.badar.llp.Responses;
 
 public class LanguageResponse {
     String languageName;
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
 }
