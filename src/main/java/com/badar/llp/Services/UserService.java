0e9fed526b1bd2d0c01c2d1f8c30bb34093fbb7d
Badar <bader5uk@hotmail.com>
2025-03-03 10:11:17 +0400

    Adjusted getUserInfo function to reflect changes

--- src/main/java/com/badar/llp/Services/UserService.java
+++ src/main/java/com/badar/llp/Services/UserService.java
@@ -1,10 +1,9 @@
 package com.badar.llp.Services;
 
 import com.badar.llp.DTOs.UserDTO;
-import com.badar.llp.Models.Language;
-import com.badar.llp.Models.Role;
-import com.badar.llp.Models.User;
-import com.badar.llp.Models.Video;
+import com.badar.llp.Models.*;
+import com.badar.llp.Repositories.StudentRepository;
+import com.badar.llp.Repositories.TutorRepository;
 import com.badar.llp.Repositories.UserRepository;
 import com.badar.llp.Responses.LanguageResponse;
 import com.badar.llp.Responses.UserResponse;
@@ -19,6 +18,9 @@ import java.util.Date;
 import java.util.EnumSet;
 import java.util.List;
 
+import static com.badar.llp.Models.Role.STUDENT;
+import static com.badar.llp.Models.Role.TUTOR;
+
 @Service
 public class UserService {
 
@@ -26,12 +28,18 @@ public class UserService {
     UserRepository userRepository;
 
     @Autowired
+    TutorRepository tutorRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     JwtService jwtService;
 
-    public UserDTO signup(UserDTO dto){
+    public UserDTO signup(UserDTO dto) {
         User newUser = UserDTO.convertFromDTO(dto);
         newUser.setCreatedDate(new Date());
         newUser.setActive(true);
@@ -43,41 +51,54 @@ public class UserService {
 
     public String login(UserDTO dto) throws Exception {
         User user = userRepository.findByUserName(dto.getUserName());
-        if(HelperUtils.isNull(user) || !user.getActive()){
+        if (HelperUtils.isNull(user) || !user.getActive()) {
             throw new Exception("User not valid");
         }
-        if(!bCryptPasswordEncoder.matches(dto.getPassword(), user.getPassword())){
+        if (!bCryptPasswordEncoder.matches(dto.getPassword(), user.getPassword())) {
             throw new Exception("User not valid");
 
         }
         return jwtService.generateToken(dto);
     }
-    public List<Role> getRoles(){
+
+    public List<Role> getRoles() {
         List<Role> roleList = new ArrayList<Role>(EnumSet.allOf(Role.class));
         return roleList;
     }
 
-    public UserResponse getUserInfo(String userName){
+    public UserResponse getUserInfo(String userName) {
         User user = userRepository.findByUserName(userName);
         UserResponse response = new UserResponse();
         response.setName(user.getName());
         response.setUserName(userName);
         response.setEmail(user.getEmail());
         response.setRole(user.getRole());
-        List<VideoResponse> vResponse = new ArrayList<>();
-        for(Video video: user.getVideoList()){
-            VideoResponse videoResponse = new VideoResponse();
-            videoResponse.setLink(video.getLink());
-            videoResponse.setTitle(video.getName());
-            vResponse.add(videoResponse);
+        if (user.getRole() == TUTOR) {
+            List<VideoResponse> vResponse = new ArrayList<>();
+            Tutor tutor = tutorRepository.findByUserName(userName);
+            for (Video video : tutor.getVideoList()) {
+                VideoResponse videoResponse = new VideoResponse();
+                videoResponse.setLink(video.getLink());
+                videoResponse.setTitle(video.getName());
+                vResponse.add(videoResponse);
+
+                response.setVideoList(vResponse);
+                List<LanguageResponse> lResposne = new ArrayList<>();
+                for (Language language : tutor.getLanguageList()) {
+                    LanguageResponse languageResponse = new LanguageResponse();
+                    languageResponse.setLanguageName(language.getName());
+                }
+                response.setLanguageList(lResposne);
+            }
+        } else if (user.getRole() == STUDENT) {
+            Student student = studentRepository.findByUserName(userName);
+            List<LanguageResponse> lResposne = new ArrayList<>();
+            for (Language language : student.getLanguageList()) {
+                LanguageResponse languageResponse = new LanguageResponse();
+                languageResponse.setLanguageName(language.getName());
+            }
+            response.setLanguageList(lResposne);
         }
-        response.setVideoList(vResponse);
-        List<LanguageResponse> lResposne = new ArrayList<>();
-        for(Language language: user.getLanguageList()){
-            LanguageResponse languageResponse = new LanguageResponse();
-            languageResponse.setLanguageName(language.getName());
-        }
-        response.setLanguageList(lResposne);
         return response;
     }
 }
