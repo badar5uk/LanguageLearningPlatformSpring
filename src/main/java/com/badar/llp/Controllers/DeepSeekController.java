82c93e43147437e9f6323d2a9d7d6fe359a72173
Badar <bader5uk@hotmail.com>
2025-03-09 12:13:41 +0400

    Added AiService and adjust AI controller

--- src/main/java/com/badar/llp/Controllers/DeepSeekController.java
+++ src/main/java/com/badar/llp/Controllers/DeepSeekController.java
@@ -1,6 +1,8 @@
 package com.badar.llp.Controllers;
 
+import com.badar.llp.Services.AiService;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -10,37 +12,19 @@ import reactor.core.publisher.Flux;
 @CrossOrigin(origins = "*")
 public class DeepSeekController {
 
-    private final ChatClient chatClient;
+    //private final ChatClient chatClient;
 
-    public DeepSeekController(ChatClient.Builder chatClient) {
-        this.chatClient = chatClient.build();
-    }
+    @Autowired
+    AiService aiService;
+
 
     @GetMapping("/{chat}")
     public String promptWithPathVariable(@PathVariable String chat) {
         try {
-            String response = chatClient
-                    .prompt(chat)
-                    .call()
-                    .content();
-            return parseLLMResponse(response);
+            return aiService.getAIHelp(chat);
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
-    }
-
-    @GetMapping("/stream/{chat}")
-    public Flux<String> streamChat(@PathVariable String chat) {
-        return chatClient
-                .prompt()
-                .user(chat)
-                .stream()
-                .content();
-    }
-
-    String parseLLMResponse(String rawInput) {
-        Integer index = rawInput.indexOf("</think>");
-        return rawInput.substring(index + "</think>".length() + 2).replace("\n", "");
     }
 }
 
